2182bfb8755e17f1891261474ee34e301165a207
Mario Junior <mariojuniortrab@gmail.com>
2024-07-09 17:15:13 -0300

    refact: move yargs config to an adapter class

--- cnabRows.js
+++ cnabRows.js
@@ -5,22 +5,14 @@ import { readFile } from 'fs/promises'
 import { fileURLToPath } from 'url'
 import Logger from './logger.js'
 import CnabFileManager from './cnabFileManager.js'
+import YargsAdapter from './yargsAdapter.js';
 
-import yargs from 'yargs'
-
-const optionsYargs = yargs(process.argv.slice(2))
-  .usage('Uso: $0 [options]')
-  .option("f", { alias: "from", describe: "posição inicial de pesquisa da linha do Cnab", type: "number", demandOption: true })
-  .option("t", { alias: "to", describe: "posição final de pesquisa da linha do Cnab", type: "number", demandOption: true })
-  .option("s", { alias: "segmento", describe: "tipo de segmento", type: "string", demandOption: true })
-  .example('$0 -f 21 -t 34 -s p', 'lista a linha e campo que from e to do cnab')
-  .argv;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const file = path.resolve(`${__dirname}/cnabExample.rem`)
 
-const { from, to, segmento } = optionsYargs
+const { from, to, segmento } = YargsAdapter.getOptions()
 
 console.time('leitura Async')
 
---
+++ yargsAdapter.js
@@ -0,0 +1,14 @@
+import yargs from 'yargs'
+
+export default class YargsAdapter {
+
+  static getOptions = () => {
+    return yargs(process.argv.slice(2))
+      .usage('Uso: $0 [options]')
+      .option("f", { alias: "from", describe: "posição inicial de pesquisa da linha do Cnab", type: "number", demandOption: true })
+      .option("t", { alias: "to", describe: "posição final de pesquisa da linha do Cnab", type: "number", demandOption: true })
+      .option("s", { alias: "segmento", describe: "tipo de segmento", type: "string", demandOption: true })
+      .example('$0 -f 21 -t 34 -s p', 'lista a linha e campo que from e to do cnab')
+      .argv;
+  }
+}
